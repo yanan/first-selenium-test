4c82b76b588795b69ed6b5a3bb1136f4bfcc62c1
Ashley Yuan <yanan.yuan@liferay.com>
2022-03-20 22:16:31 +0800

    Add test files

---
+++ src/test/java/com/liferay/forms/ui/UI.java
@@ -0,0 +1,35 @@
+package com.liferay.forms.ui;
+
+public interface UI {
+	String ALERT_SUCCESS = "Success:Your request completed successfully.";
+
+	String DESCRIPTION_FIELD = "This is a description field from our Liferay Forms";
+
+	String FIELD_IS_REQUIRED = "This field is required";
+
+	String FIRST_PAGE_OF_FORMS = "This is the first page of our forms.";
+
+	String FORMS_URL = "https://forms.liferay.com/web/forms/shared/-/form/122548";
+
+	String INDICATES_REQUIRED_FIELDS = "Indicates Required Fields";
+
+	String LIFERAY_FORM_HEADER = "This is a Liferay Forms";
+
+	String LIFERAY_FORM_TITLE = "This is a Liferay Forms";
+
+	String PARTY_ROCK = "Let's party rock.";
+
+	String PORTUGUES_HEADER = "Este é um Liferay Forms";
+
+	String RECEIVED_SUCCESSFULLY = "Your information was successfully received. Thank you for filling out the form.";
+
+	String SECOND_PAGE_TITLE ="Form - Forms";
+
+	String SUBMETER = "Submeter";
+
+	String WHAT_IS_DATE_OF_BIRTH = "What is the date of your birth?";
+
+	String WHAT_IS_YOUR_NAME = "What is your name?";
+
+	String WHY_JOIN_TESTING_AREA = "Why did you join the testing area?";
+}
